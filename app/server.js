56f1fff17fbbb1bb9df960b2a4bb4242b968f680
Mukul <mukul18@gmail.com>
2020-10-06 11:35:00 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ app.use(morgan('combined'));
 // Configuration
 var port = process.env.PORT || 8080;
 //var message = process.env.MESSAGE || "Hello world!";
-var message = "Hello world!";
+var message = "Hello Kubernetes!";
 
 app.get('/', function (req, res) {
     res.render('home', {
